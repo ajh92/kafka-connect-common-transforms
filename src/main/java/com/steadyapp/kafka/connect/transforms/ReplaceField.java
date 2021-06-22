cc08caf2cf29478753edf39108322ab91e49c0a8
Andrew Hill <7014855+ajh92@users.noreply.github.com>
2021-06-22 16:31:06 -0500

    Allow null values in nested object

--- src/main/java/com/steadyapp/kafka/connect/transforms/ReplaceField.java
+++ src/main/java/com/steadyapp/kafka/connect/transforms/ReplaceField.java
@@ -174,7 +174,7 @@ public abstract class ReplaceField<R extends ConnectRecord<R>> implements Transf
 
       Object fieldValue = value.get(reverseRenamed(fieldName));
 
-      if (field.schema().type() == Schema.Type.STRUCT) {
+      if (field.schema().type() == Schema.Type.STRUCT && fieldValue != null) {
         Struct structValue = requireStruct(fieldValue, PURPOSE);
         fieldValue = expandStruct(field.schema(), structValue, fieldName);
       }
